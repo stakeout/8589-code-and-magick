1088d3c76b2ab5b2d17a9a03b66eef927a107cdd
Vadim Novash <novash@tut.by>
2018-09-08 15:46:13 +0300

    fixes #2

--- js/stat.js
+++ js/stat.js
@@ -12,6 +12,9 @@ var BAR_STEP = 50; // расстояние между столбиками
 var TEXT = 'Ура! Вы победили!';
 var TEXT_TITLE = 'Список результатов:';
 var TEXT_COLOR = '#000';
+var RED = 'rgba(255, 0, 0, 1)';
+var RGBA_BLACK_0_7 = 'rgba(0, 0, 0, .7)';
+var WHITE_COLOR = '#fff';
 
 // Функция отрисовки облака
 var renderCloud = function (ctx, x, y, color) {
@@ -26,7 +29,6 @@ var renderCloud = function (ctx, x, y, color) {
   ctx.lineTo(x, y + CLOUD_HEIGHT);
   ctx.lineTo(x - STEP * 3, y + CLOUD_HEIGHT / 2);
   ctx.fill();
-
 };
 
 // Функция нахождения максимального элемента в массиве
@@ -44,17 +46,11 @@ var renderText = function (ctx, color, font, textBaseline, text, x, y) {
   ctx.fillText(text, x, y);
 };
 
-// Функция нахождения случайного числа
-
-function getRandomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min) / 10;
-}
-
 // Функция нахождения случайного оттенка
 
 var getBlueShade = function () {
-  var blueShade = 'rgba(0, 0, 255, ' + Math.random() + ')';
-  return blueShade;
+  return 'rgba(0, 0, 255, ' + Math.random() + ')';
+
 };
 
 // Функция отрисовки столбиков
@@ -71,9 +67,9 @@ var renderBar = function (ctx, color, name, time, index, barHeight, x) {
 
 window.renderStatistics = function (ctx, names, times) {
 
-  renderCloud(ctx, CLOUD_POSITION_X + STEP, CLOUD_POSITION_Y + STEP, 'rgba(0, 0, 0, .7)');
+  renderCloud(ctx, CLOUD_POSITION_X + STEP, CLOUD_POSITION_Y + STEP, RGBA_BLACK_0_7);
 
-  renderCloud(ctx, CLOUD_POSITION_X, CLOUD_POSITION_Y, '#fff');
+  renderCloud(ctx, CLOUD_POSITION_X, CLOUD_POSITION_Y, WHITE_COLOR);
 
   renderText(ctx, TEXT_COLOR, '16px PT Mono', 'top', TEXT, CLOUD_POSITION_X + STEP * 3, CLOUD_POSITION_Y + STEP * 1.5);
 
@@ -83,7 +79,7 @@ window.renderStatistics = function (ctx, names, times) {
 
   for (var i = 0; i < names.length; i++) {
     var barHeight = HISTOGRAM_HEIGHT * times[i] / maxTime;
-    var barColor = names[i] === 'Вы' ? 'rgba(255, 0, 0, 1)' : getBlueShade();
+    var barColor = names[i] === 'Вы' ? RED : getBlueShade();
     renderBar(ctx, barColor, names[i], Math.floor(times[i]), i, barHeight, CLOUD_POSITION_X);
   }
 };
